d5c77222d174d608cb2057c54c5ff015bbb9d14b
vul3cl41up <138540058+vul3cl41up@users.noreply.github.com>
2023-08-28 17:12:17 +0800

    角色移動

---
+++ Assets/Script/Role_Child_Control_Magic.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+
+public class Role_Child_Control_Magic : MonoBehaviour
+{
+    GameObject m_Parent;
+    Role_Control_Magic parent_script;
+    Animator ani;
+    GameObject role_now;
+    GameObject role_idle;
+    GameObject role_walk;
+    GameObject role_jump;
+
+    private void Start()
+    {
+        m_Parent = transform.parent.gameObject;
+        parent_script = m_Parent.GetComponent<Role_Control_Magic>();
+        role_idle = transform.GetChild(0).gameObject;
+        role_walk = transform.GetChild(1).gameObject;
+        role_jump = transform.GetChild(2).gameObject;
+        role_now = role_idle;
+    }
+
+    private void OnEnable()
+    {
+        if (parent_script.Status_Now == Role_Control_Magic.Status.Idle)
+        {
+            role_now = role_idle;
+            role_now.SetActive(true);
+        }
+        else if (parent_script.Status_Now == Role_Control_Magic.Status.Walk)
+        {
+            role_now = role_walk;
+            role_now.SetActive(true);
+        }
+        else if(parent_script.Status_Now == Role_Control_Magic.Status.Jump)
+        {
+            role_now = role_jump;
+            role_now.SetActive(true);
+        }
+    }
+
+    private void OnDisable()
+    {
+        role_now.SetActive(false);
+    }
+
+    private void FixedUpdate()
+    {
+        ChangeAni();
+    }
+
+    void ChangeAni()
+    {
+        if(parent_script.Status_Now == Role_Control_Magic.Status.Jump)
+        {
+            if (role_now != role_jump)
+            {
+                role_now.SetActive(false);
+                role_now = role_jump;
+                role_now.SetActive(true);
+            }
+        }
+        else if (parent_script.Status_Now == Role_Control_Magic.Status.Idle)
+        {
+            if(role_now != role_idle)
+            {
+                role_now.SetActive(false);
+                role_now = role_idle;
+                role_now.SetActive(true);
+            }
+        }
+        else if(parent_script.Status_Now == Role_Control_Magic.Status.Walk)
+        {
+            if (role_now != role_walk)
+            {
+                role_now.SetActive(false);
+                role_now = role_walk;
+                role_now.SetActive(true);
+            }
+        }
+
+    }
+}
